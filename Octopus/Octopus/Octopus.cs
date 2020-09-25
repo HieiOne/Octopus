d54b44d514d0dce5c35594111dc6187e39b2469f
Ivan Blasco <ivan.blasco@maxiproducts.es>
2020-09-25 11:52:42 +0200

    v0.1 - Finished config file and reading

---
+++ Octopus/Octopus/Octopus.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using Octopus.modules.messages;
+
+namespace Octopus
+{
+    class Octopus
+    {
+        static void Main(string[] args)
+        {
+            List<string> tableList;
+
+            string prefix = ConfigurationManager.AppSettings.Get("prefix") + "_";
+            string suffix = "_" + ConfigurationManager.AppSettings.Get("suffix");
+            
+            if (prefix.Length == 1) // Without value
+                prefix = null;
+
+            if (suffix.Length == 1) // Without value
+                suffix = null;
+
+            Debug.WriteLine("Reading Config file . . .");
+            tableList = ConfigurationTableList();
+
+            if (tableList.Count() != 0)
+            {
+                Debug.WriteLine("Read Config file succesfully");
+            }
+            else
+            {
+                Messages.WriteError("No tables specified in App.Config");
+            }
+
+
+        }
+
+        /// <summary>
+        /// Reads from configuration list the section TableList, converts it to a List<string> and returns it
+        /// </summary>
+        /// <returns>List<string></returns>
+        static List<string> ConfigurationTableList()
+        {
+            var applicationSettings = ConfigurationManager.GetSection("TableList") as NameValueCollection;
+            List<string> tableList = new List<string>();
+
+            if (applicationSettings.Count == 0)
+            {
+                Console.WriteLine("Application Settings are not defined");
+            }
+            else
+            {
+                foreach (var key in applicationSettings.AllKeys)
+                {
+                    tableList.Add(applicationSettings[key]);
+                }
+            }
+
+            return tableList;
+        }
+    }
+}
---
+++ Octopus/Octopus/modules/messages/Messages.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octopus.modules.messages
+{
+    class Messages
+    {
+        public static void WriteError(string value)
+        {
+            //Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(value.PadRight(Console.WindowWidth - 1)); // <-- see note
+                                                                        //
+                                                                        // Reset the color.
+                                                                        //
+            Console.ResetColor();
+        }
+
+        public static void WriteQuestion(string value)
+        {
+            //Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(value); // <-- see note
+                                      //
+                                      // Reset the color.
+                                      //
+            Console.ResetColor();
+        }
+
+        public static void WriteSuccess(string value)
+        {
+            //Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(value.PadRight(Console.WindowWidth - 1)); // <-- see note
+                                                                        //
+                                                                        // Reset the color.
+                                                                        //
+            Console.ResetColor();
+        }
+        public static void WriteExecuteQuery(string value)
+        {
+            //Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(value.PadRight(Console.WindowWidth - 1)); // <-- see note
+                                                                        //
+                                                                        // Reset the color.
+                                                                        //
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+    }
+}
