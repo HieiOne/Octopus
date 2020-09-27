66311eb9098bae82723ab0ff16bfcf79a65a44de
Ivan Blasco <blascogasconiban@gmail.com>
2020-09-28 00:06:59 +0200

    v0.37 enhanced method descriptions and select with limit

--- Octopus/Octopus/modules/dbModules/DataSource.cs
+++ Octopus/Octopus/modules/dbModules/DataSource.cs
@@ -8,12 +8,46 @@ namespace Octopus.modules.dbModules
 {
     public abstract class DataSource
     {
+        /// <summary>
+        /// Opens connection to the datasource
+        /// </summary>
         public abstract void Connect();
+
+        /// <summary>
+        /// Closes connection to the datasource
+        /// </summary>
         public abstract void Disconnect();
+
+        /// <summary>
+        /// SELECT query
+        /// </summary>
         public abstract void OpenReader();
+
+        /// <summary>
+        /// SELECT query with limit of results
+        /// </summary>
+        /// <param name="limit"></param>
+        public abstract void OpenReader(int limit);
+
+        /// <summary>
+        /// Closes reader used for SELECT query
+        /// </summary>
         public abstract void CloseReader();
+
+        /// <summary>
+        /// Runs any query and returns an int with the modified registers
+        /// </summary>
+        /// <returns></returns>
         public abstract int ExecuteQuery();
+
+        /// <summary>
+        /// TTSBegin in SQL
+        /// </summary>
         public abstract void BeginTransaction();
+
+        /// <summary>
+        /// TTSCommit in SQL
+        /// </summary>
         public abstract void CommitTransaction();
     }
 }
